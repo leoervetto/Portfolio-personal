ff902392b4e6e8500fc2ea7d1e5fc54395219e5b
leoervetto <leoervetto@gmail.com>
2024-09-11 18:43:48 -0300

    Configurado la barra de desplazamiento personalizada, y generados efectos en el header

---
+++ js/main.js
@@ -0,0 +1,6 @@
+//Navigation Bar effects on scroll
+window.addEventListener('scroll', function(){
+    const header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+    
+})
