8cfd1aae7e2ee80f42676c190fcffd9bcf2296eb
leoervetto <leoervetto@gmail.com>
2024-09-12 20:46:41 -0300

    Agregado Servicios, falta corregir los textos

--- js/main.js
+++ js/main.js
@@ -4,3 +4,27 @@ window.addEventListener('scroll', function(){
     header.classList.toggle('sticky', window.scrollY > 0);
     
 })
+
+//Services section modal
+
+const serviceModals = document.querySelectorAll('.service-modal');
+const learnMoreBtns = document.querySelectorAll('.learn-more-btn');
+const modalCloseBtns = document.querySelectorAll(".modal-close-btn");
+
+var modal = function(modalClick){
+    serviceModals[modalClick].classList.add('active')
+};
+
+learnMoreBtns.forEach((learnMoreBtn, i) => {
+    learnMoreBtn.addEventListener("click", () => {
+        modal(i)
+    });
+});
+
+modalCloseBtns.forEach((modalCloseBtn) => {
+    modalCloseBtn.addEventListener("click", () => {
+        serviceModals.forEach((modalView) => {
+            modalView.classList.remove("active");
+        });
+    });
+});
